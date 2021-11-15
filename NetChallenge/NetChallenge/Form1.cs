696a09ca936aaa2c0e63aeb530b84f8b6b6f5557
satriamilan <satriamilan@gmail.com>
2021-11-16 00:12:10 +0700

    asdadasdad

---
+++ NetChallenge/NetChallenge/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetChallenge
+{
+    public partial class Form1 : Form
+    {
+        private int formid = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            formid++;
+            MainForm mform = new MainForm(formid.ToString());
+            mform.Show();
+           // mform.Focus();
+            this.Hide();
+        }
+    }
+}
